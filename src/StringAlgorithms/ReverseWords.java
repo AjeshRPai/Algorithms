dbcc15afb9486910b1a83461e3b34e58e895211d
Ajesh R Pai <ajeshr2k1@gmail.com>
2018-01-05 13:07:52 +0530

    Reverse words in a String

---
+++ src/StringAlgorithms/ReverseWords.java
@@ -0,0 +1,19 @@
+package StringAlgorithms;
+
+public class ReverseWords{
+    
+    public static void main(String[] args) {
+        String string="i.like.this.program.very.much";
+        reverse(string);
+    }
+    
+    
+    private static void reverse(String string){
+        String reversed[]=string.split("\\.");
+        StringBuffer reversed_string=new StringBuffer();
+        for(int i=reversed.length-1;i>=0;i--){
+            reversed_string.append(reversed[i]).append(".");
+        }
+        System.out.println(reversed_string.toString());
+    }
+}
