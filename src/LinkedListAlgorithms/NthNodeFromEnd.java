c45deb5c780dce4ed7f21d1d86bcd1cbe57e8997
Ajesh R Pai <ajeshr2k1@gmail.com>
2018-01-06 07:44:37 +0530

    Nth node from the End

---
+++ src/LinkedListAlgorithms/NthNodeFromEnd.java
@@ -0,0 +1,21 @@
+package LinkedListAlgorithms;
+
+public class NthNodeFromEnd {
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        for (int i = 8; i > 0; --i) {
+            llist.push(i);
+        }
+        printNthNode(llist.head, 5);
+    }
+    
+    private static int printNthNode(LinkedList.Node head, int i) {
+        if (head == null)
+            return 0;
+        int index=printNthNode(head.next, i)+1;
+        if(index == i){
+             System.out.println("The item is "+head.data);
+        }
+        return index;
+    }
+}
--- src/StringAlgorithms/Permutation.java
+++ src/StringAlgorithms/Permutation.java
@@ -3,17 +3,19 @@ package StringAlgorithms;
 public class Permutation {
     
     public static void main(String[] args) {
-        String string="DFGHJ";
+        String string="ABC";
         computePermutation(string,0,string.length()-1);
     }
     
    
-    private static void computePermutation(String str, int l, int r)
-    {
-        if (l == r)
-            System.out.println(str);
+    private static void computePermutation(String str, int l, int r) {
+        System.out.println("Str "+str+" l="+l);
+        if (l == r){
+        
+        }
         else {
             for (int i = l; i <= r; i++) {
+                System.out.println("i "+i);
                 str = swap(str,l,i);
                 computePermutation(str, l+1, r);
                 str = swap(str,l,i);
