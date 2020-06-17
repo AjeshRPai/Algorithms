278feed321fc0ecbc85a29227f957b15067623c3
Ajesh R Pai <ajeshr2k1@gmail.com>
2020-06-17 09:59:15 +0530

    [+] Graph Theory Introduction

--- src/GraphAlgorithms/DepthFirstSearch.java
+++ src/GraphAlgorithms/DepthFirstSearch.java
@@ -3,12 +3,9 @@ package GraphAlgorithms;
 public class DepthFirstSearch {
     
     static void DFSUtil(int vertex,boolean visited[],Graph graph){
-        
         visited[vertex]=true;
-    
         System.out.print(vertex+" ");
-        
-        for (Integer n : (Iterable<Integer>) graph.getAdj()[vertex]) {
+        for (Integer n : graph.getAdj()[vertex]) {
             if (!visited[n]) {
                 DFSUtil(n,visited,graph);
             }
@@ -22,14 +19,18 @@ public class DepthFirstSearch {
     
     public static void main(String[] args){
         Graph g = new Graph(4); //Graph with 4 edges
-    
+
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 2);
         g.addEdge(2, 0);
         g.addEdge(2, 3);
         g.addEdge(3, 3);
-    
+
+        System.out.println("graph = " + g);
+
+        g.printGraph();
+
         System.out.println("Following is Depth First Traversal "+
                 "(starting from vertex 2)");
     
--- src/GraphAlgorithms/Graph.java
+++ src/GraphAlgorithms/Graph.java
@@ -1,16 +1,18 @@
 package GraphAlgorithms;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Graph {
     private int v;//no of vertices
-    private LinkedList[] adj;//adjacency Lists
+    private LinkedList<Integer>[] adj;//adjacency Lists
     
     Graph(int v){
         this.v=v;
         adj=new LinkedList[v];
         for(int i=0;i<v;i++){
-            adj[i]=new LinkedList();
+            adj[i]=new LinkedList<Integer>();
         }
     }
     
@@ -18,15 +20,34 @@ public class Graph {
         return  v;
     }
     
-    public LinkedList[] getAdj() {
+    public LinkedList<Integer>[] getAdj() {
         return adj;
     }
-    
-    
     
     public void addEdge(int from_vertex, int to_vertex){
         adj[from_vertex].add(to_vertex);
     }
-    
-    
+
+    @Override
+    public String toString() {
+        return "Graph{" +
+                "vertices  =" + v +
+                ", adj=" + Arrays.toString(adj) +
+                '}';
+    }
+
+    public void printGraph(){
+        int vertex = getVertices();
+        List<Integer>[] list = getAdj();
+
+        for (int i = 0; i <vertex ; i++) {
+            if(list[i].size()>0) {
+                System.out.print("Vertex " + i + " is connected to: ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
