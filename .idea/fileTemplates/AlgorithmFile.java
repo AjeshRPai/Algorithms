6cf6b135dc88a629690db6576a6f8ef4daa742e9
Ajesh R Pai <ajeshr2k1@gmail.com>
2020-06-19 08:34:08 +0530

    [+] Dungeon master problem

---
+++ .idea/fileTemplates/AlgorithmFile.java
@@ -0,0 +1,14 @@
+
+
+import static java.lang.System.out;
+
+//Created by Ajesh on ${DATE}
+public class ${Name}{
+   public static void main(String[] args){
+   
+   }
+   
+   private static ${return_type} get_${MethodName}(${Arguments}){
+   
+   }
+}
--- src/GraphAlgorithms/DungeonMasterProblem.java
+++ src/GraphAlgorithms/DungeonMasterProblem.java
@@ -1,4 +1,28 @@
 package GraphAlgorithms;
 
 public class DungeonMasterProblem {
+
+    static void initialize() {
+        int R = 3;
+        int C = 4;
+
+        int startRow;
+        int startColumn;
+
+        int rowQueue;
+        int columnQueue;
+
+        int moveCount;
+        int nodesLeftInLayer;
+        int nodesLeftInNextLayer;
+
+        boolean reachedEnd;
+
+        boolean visited;
+
+//      To move to the direction of North, east, west, south
+        int[] dr = new int[]{-1,+1,0,0};
+        int[] dc = new int[]{0,0,+1,-1};
+
+    }
 }
