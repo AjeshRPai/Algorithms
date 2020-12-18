63988cac4c3102952cb3969255e473021897b0f5
Ajesh Pai <ajesh.pai@capitalfloat.com>
2020-12-18 08:44:39 +0530

    [+] Valid BST or not

--- src/BinaryTreeAlgorithms/BSTCheck.java
+++ src/BinaryTreeAlgorithms/BSTCheck.java
@@ -1,34 +1,49 @@
 package BinaryTreeAlgorithms;
 
-import BinaryTreeAlgorithms.BinaryTree.*;
 
 public class BSTCheck {
-    
-    
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        tree.root = new TreeNode(4);
-        tree.root.left = new TreeNode(6);
-        tree.root.right = new TreeNode(5);
-        tree.root.left.left = new TreeNode(1);
-        tree.root.left.right = new TreeNode(3);
-        
-        System.out.println("The tree is bst = "+isBst(tree.root));
+
+	public static void main(String[] args) {
+
+		BST root = new BST(10);
+		root.left = new BST(5);
+		root.left.left = new BST(2);
+		root.left.left.left = new BST(1);
+		root.left.right = new BST(5);
+		root.right = new BST(15);
+		root.right.left = new BST(13);
+		root.right.left.right = new BST(14);
+		root.right.right = new BST(22);
+
+		boolean b = validateBst(root);
+		System.out.println("b = " + b);
+	}
+
+	static class BST {
+		public int value;
+		public BST left;
+		public BST right;
+
+		public BST(int value) {
+			this.value = value;
+		}
+	}
+
+
+    public static boolean validateBst(BST tree) {
+	    return isBstUtil(tree,Integer.MIN_VALUE,Integer.MAX_VALUE);
     }
-    
-    private static boolean isBst(TreeNode root) {
-        return isBstUtil(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
-    }
-    
-    private static boolean isBstUtil(TreeNode node, int minValue, int maxValue) {
-        if(node==null)return true;
-        
-        if(node.key<minValue || node.key>maxValue)
+
+    private static boolean isBstUtil(BST node, int minValue, int maxValue) {
+	    System.out.println("node = " + node + ", minValue = " + minValue + ", maxValue = " + maxValue);
+        if(node==null)
+        	return true;
+
+        if(node.value<minValue || node.value>=maxValue)
             return false;
-        
-        return (isBstUtil(node.left,minValue,node.key-1)&&
-                isBstUtil(node.right,node.key+1,maxValue));
+
+        return (isBstUtil(node.left,minValue,node.value)&&
+                isBstUtil(node.right,node.value,maxValue));
     }
-    
-    
+
 }
